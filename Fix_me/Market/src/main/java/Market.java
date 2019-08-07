dafdd988d93b7b219ccc36259ca051cd01f3b318
kmarchan <kmarchan@student.wethinkcode.co.za>
2019-08-07 19:44:34 +0200

    .

--- Fix_me/Market/src/main/java/Market.java
+++ Fix_me/Market/src/main/java/Market.java
@@ -8,24 +8,6 @@ import java.net.Socket;
 public class Market {
 	public static final String ANSI_BLUE = "\u001B[34m";
 	public static void main(String[] args) {
-		try(ServerSocket serverSocket = new ServerSocket(5000)) {
-			Socket socket = serverSocket.accept();
-			System.out.println(ANSI_BLUE + "Client Connected");
-			BufferedReader input = new BufferedReader(
-					new InputStreamReader(socket.getInputStream()));
-			PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
-
-			while(true) {
-				String echoString = input.readLine();
-				if(echoString.equals(ANSI_BLUE + "exit")) {
-					break;
-				}
-				output.println(ANSI_BLUE + "Echo from server: " + echoString);
-			}
-
-
-		} catch(IOException e) {
-			System.out.println(ANSI_BLUE + "Server exception " + e.getMessage());
-		}
+		System.out.println(ANSI_BLUE + "Hello World, from the router");
 	}
 }
--- Fix_me/Router/src/main/java/Router.java
+++ Fix_me/Router/src/main/java/Router.java
@@ -1,7 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Router {
 	public static final String ANSI_CYAN = "\u001B[36m";
+	static final int brokerPort = 5000;
+	static final int marketPort = 5001;
 	public static void main(String[] args) {
-		System.out.println(ANSI_CYAN + "Hello World, from the router");
+		try(ServerSocket serverSocket = new ServerSocket(brokerPort)) {
+			Socket socket = serverSocket.accept();
+			System.out.println(ANSI_CYAN + "Client Connected");
+			BufferedReader input = new BufferedReader(
+					new InputStreamReader(socket.getInputStream()));
+			PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
+
+			while(true) {
+				String echoString = input.readLine();
+				if(echoString.equals(ANSI_CYAN + "exit")) {
+					break;
+				}
+				output.println(ANSI_CYAN + "Echo from server: " + echoString);
+			}
+
+
+		} catch(IOException e) {
+			System.out.println(ANSI_CYAN + "Server exception " + e.getMessage());
+		}
 	}
 }
