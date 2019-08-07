019a6101ac7e79458e30bffd1ab06bb461873c97
kmarchan <kmarchan@student.wethinkcode.co.za>
2019-08-07 12:36:22 +0200

    following Java Networking Udemy tut

--- Fix_me/Broker/src/main/java/Broker.java
+++ Fix_me/Broker/src/main/java/Broker.java
@@ -1,7 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class Broker {
-	public static final String ANSI_BLUE = "\u001B[34m";
-	public static void main( String[] args )
-    {
-        System.out.println( ANSI_BLUE + "Hello World!" );
-    }
+	public static final String ANSI_PURPLE = "\u001B[35m";
+	public static void main(String[] args) {
+		try (Socket socket = new Socket("localhost", 5000)) {
+			BufferedReader echoes = new BufferedReader(
+					new InputStreamReader(socket.getInputStream()));
+			PrintWriter stringToEcho = new PrintWriter(socket.getOutputStream(), true);
+
+			Scanner scanner = new Scanner(System.in);
+			String echoString;
+			String response;
+
+			do {
+				System.out.println(ANSI_PURPLE + "Enter string to be echoed: ");
+				echoString = scanner.nextLine();
+
+				stringToEcho.println(echoString);
+				if(!echoString.equals(ANSI_PURPLE + "exit")) {
+					response = echoes.readLine();
+					System.out.println(response);
+				}
+			} while(!echoString.equals(ANSI_PURPLE + "exit"));
+
+		} catch (IOException e) {
+			System.out.println(ANSI_PURPLE + "Client Error: " + e.getMessage());
+
+		}
+	}
 }
--- Fix_me/Market/src/main/java/Market.java
+++ Fix_me/Market/src/main/java/Market.java
@@ -1,6 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Market {
-	public static final String ANSI_PURPLE = "\u001B[35m";
+	public static final String ANSI_BLUE = "\u001B[34m";
 	public static void main(String[] args) {
-		System.out.println(ANSI_PURPLE + "Hello World Market");
+		try(ServerSocket serverSocket = new ServerSocket(5000)) {
+			Socket socket = serverSocket.accept();
+			System.out.println(ANSI_BLUE + "Client Connected");
+			BufferedReader input = new BufferedReader(
+					new InputStreamReader(socket.getInputStream()));
+			PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
+
+			while(true) {
+				String echoString = input.readLine();
+				if(echoString.equals(ANSI_BLUE + "exit")) {
+					break;
+				}
+				output.println(ANSI_BLUE + "Echo from server: " + echoString);
+			}
+
+
+		} catch(IOException e) {
+			System.out.println(ANSI_BLUE + "Server exception " + e.getMessage());
+		}
 	}
 }
