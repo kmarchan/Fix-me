e6e1e1a13aa6a3076c34046f3d2e329959e61079
kmarchan <kmarchan@student.wethinkcode.co.za>
2019-08-10 19:17:08 +0200

    file structure

--- Fix_me/Router/src/main/java/Router.java
+++ Fix_me/Router/src/main/java/Router.java
@@ -5,11 +5,19 @@ public class Router {
 	public static final String ANSI_CYAN = "\u001B[36m";
 	static final int brokerPort = 5000;
 	static final int marketPort = 5001;
+
+	// if (MarketPort) {
+	// call MarketListener
+	// }
+	// else if (BrokerPort) {
+	// 	new MarketListener(serverSocket.accept()).start();
+	// }
+
 	public static void main(String[] args) {
 		try(ServerSocket serverSocket = new ServerSocket(brokerPort)) {
 			while(true) {
 				System.out.println(ANSI_CYAN + "Client Connected");
-				new MarketListener(serverSocket.accept()).start();
+				new BrokerListener(serverSocket.accept()).start();
 			}
 		} catch(IOException e) {
 			System.out.println(ANSI_CYAN + "Server exception " + e.getMessage());
