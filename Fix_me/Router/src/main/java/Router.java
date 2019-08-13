e0a183512a5412ebc8f0069f00385f4319397d81
kmarchan <kmarchan@student.wethinkcode.co.za>
2019-08-13 19:51:29 +0200

    need to change to asynchronousServerSocketChannel.

--- Fix_me/Router/src/main/java/Router.java
+++ Fix_me/Router/src/main/java/Router.java
@@ -1,5 +1,7 @@
 import java.io.IOException;
+import java.net.InetSocketAddress;
 import java.net.ServerSocket;
+import java.nio.channels.AsynchronousServerSocketChannel;
 
 public class Router {
 	private static final String ANSI_CYAN = "\u001B[36m";
@@ -7,6 +9,11 @@ public class Router {
 	private static final int marketPort = 5001;
 
 	public static void main(String[] args) {
+//		try {
+//			final AsynchronousServerSocketChannel listener = AsynchronousServerSocketChannel.open().bind(new InetSocketAddress(5000));
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
 		while (true) {
 			try (ServerSocket serverSocket = new ServerSocket(brokerPort)) {
 				new SocketManager(serverSocket.accept()).start();
